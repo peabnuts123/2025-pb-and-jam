77eda28990dcf92f75951e1d8e677a0483390913
peabnuts123 <peabnuts123@gmail.com>
2025-04-27 14:43:25 +1200

    Add music to main menu, decision screen, spend coin sfx

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -9,13 +9,16 @@ extends Node2D
 @onready var productivity_upgrade_button = $CanvasLayer/Container/Stats/Productivity/Button
 @onready var momentum_upgrade_button = $CanvasLayer/Container/Stats/Momentum/Button
 
-@onready var game_over_music = $CanvasLayer/GameOverMusic
+@onready var game_over_music = $GameOverMusic
+@onready var normal_music = $NormalMusic
+@onready var spend_coin_sound = $SpendCoin
 
 func _ready():
 	_update_ui()
 	if not SaveData.game_over_message.is_empty():
 		game_over_music.play()
-	#else @TODO
+	else:
+		normal_music.play()
 
 func _on_play_pressed():
 	SaveData.start_new_run()
@@ -25,18 +28,21 @@ func _on_upgrade_wellbeing():
 	if SaveData.coin >= Content.upgrade_cost and SaveData.stat_base_wellbeing < Content.stats_max_value:
 		SaveData.coin -= Content.upgrade_cost;
 		SaveData.stat_base_wellbeing += 1
+		spend_coin_sound.play()
 		_update_ui()
 
 func _on_upgrade_productivity():
 	if SaveData.coin >= Content.upgrade_cost and SaveData.stat_base_productivity < Content.stats_max_value:
 		SaveData.coin -= Content.upgrade_cost;
 		SaveData.stat_base_productivity += 1
+		spend_coin_sound.play()
 		_update_ui()
 
 func _on_upgrade_momentum():
 	if SaveData.coin >= Content.upgrade_cost and SaveData.stat_base_momentum < Content.stats_max_value:
 		SaveData.coin -= Content.upgrade_cost;
 		SaveData.stat_base_momentum += 1
+		spend_coin_sound.play()
 		_update_ui()
 
 func _update_ui():
@@ -49,7 +55,7 @@ func _update_ui():
 	wellbeing_upgrade_button.disabled = SaveData.stat_base_wellbeing >= Content.stats_max_value or SaveData.coin < Content.upgrade_cost
 	productivity_upgrade_button.disabled = SaveData.stat_base_productivity >= Content.stats_max_value or SaveData.coin < Content.upgrade_cost
 	momentum_upgrade_button.disabled = SaveData.stat_base_momentum >= Content.stats_max_value or SaveData.coin < Content.upgrade_cost
-	
+
 	# Bind all the buttons to say the correct cost based on config
 	wellbeing_upgrade_button.text = " +1 (%d coin) " % Content.upgrade_cost
 	productivity_upgrade_button.text = " +1 (%d coin) " % Content.upgrade_cost
