9b0b46c40c271548ce2ee25af98b51c8f725e027
celerydog <lizzy.eden@gmail.com>
2025-04-27 14:40:10 +1200

    boss fires immediately

--- scripts/gameplay/bullet-hell/finite_state_machine.gd
+++ scripts/gameplay/bullet-hell/finite_state_machine.gd
@@ -4,7 +4,7 @@ var current_state: State
 var previous_state: State
 
 func _ready():
-	current_state = get_child(0) as State
+	current_state = get_child(1) as State
 	previous_state = current_state
 	current_state.enter()
 
