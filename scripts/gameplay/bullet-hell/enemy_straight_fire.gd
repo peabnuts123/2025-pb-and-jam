6fc69ae952d20f093ac66c75110e1c6e5462c415
peabnuts123 <peabnuts123@gmail.com>
2025-04-27 17:26:08 +1200

    Drop coins for killing enemies

--- scripts/gameplay/bullet-hell/enemy_straight_fire.gd
+++ scripts/gameplay/bullet-hell/enemy_straight_fire.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 @export var bullet_node: PackedScene
 
+@onready var sprite = $Sprite2D
+@onready var death_sound = $DeathSound
+
 var health = 0
+var is_dead = false;
 
 func _ready():
 	var current_level = max(0, SaveData.current_run_level - 1)
@@ -25,4 +29,12 @@ func shoot():
 		get_tree().current_scene.call_deferred("add_child", bullet)
 
 func die():
+	if is_dead:
+		return
+	is_dead = true
+	sprite.visible = false
+	SaveData.coin += Content.enemy_coins_per_death
+	death_sound.play()
+	await death_sound.finished
+	print("Death sound over")
 	queue_free()
